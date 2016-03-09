4187bb9269c6dc69770a4ee1fd73b346e7c7832a
Francis St-Amour <francis.st-amour@polymtl.ca>
2016-03-09 14:49:08 -0500

    Initial commit

---
+++ src/INF3610-Lab2-pt1 - UTF/INF3610-Lab2-pt1/Bubble.h
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//	Bubble.h
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <systemc.h>
+#include <stdio.h>
+#include "InterfaceRead.h"
+//#include "InterfaceWrite.h"		à décommenter au moment opportun
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//	Class Bubble
+//
+///////////////////////////////////////////////////////////////////////////////
+class Bubble : public sc_module
+{
+	public: 
+		// Ports    
+		/*
+		
+		À compléter
+		
+		*/
+
+	
+		// Constructor
+		Bubble( sc_module_name name );
+		
+		// Destructor
+		~Bubble();
+		
+	private:
+		// Process SystemC
+		SC_HAS_PROCESS(Bubble);
+		
+		void thread(void);
+		void bubbleSort(unsigned int *ptr, int counter);
+};
+
---
+++ src/INF3610-Lab2-pt1 - UTF/INF3610-Lab2-pt1/main.cpp
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//	main.cpp
+//
+///////////////////////////////////////////////////////////////////////////////
+#include <systemc.h>
+#include "Bubble.h"
+#include "Reader.h"
+#include "DataRAM.h"
+
+#define RAMSIZE 0x400
+
+// Global variables
+bool m_bError = false;
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//	Main
+//
+///////////////////////////////////////////////////////////////////////////////
+int sc_main(int arg_count, char **arg_value)
+{
+	// Variables
+	int sim_units = 2; //SC_NS 
+	
+	// Components
+	/*
+
+	À compléter
+
+	*/
+
+
+	
+	// Connexions
+	/*
+
+	À compléter
+
+	*/
+
+
+	// Démarrage de l'application
+	if (!m_bError)
+	{
+		cout << "Démarrage de la simulation." << endl;	
+		sc_start( -1, sc_core::sc_time_unit(sim_units) );
+		cout << endl << "Simulation s'est terminée à " << sc_time_stamp();
+	}
+	// Fin du programme
+	return 0;
+}
